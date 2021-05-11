af88cb35c2d08de3d012e436316141beb23d17fe
Craig Satchell <satchdev@gmail.com>
2021-05-10 21:07:57 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/* Day Trip Planner 
+   author: Craig Satchell
+   date: May 2021
+*/
+
+"use strict";
+
+// generate day trip
+function generateDayTrip() {
+   document.getElementById("trip").innerHTML = '<p>This area is a placehold for the trip results.</p>';
+}
