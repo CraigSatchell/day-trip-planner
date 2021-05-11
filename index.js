42110da81bc8f181413ea907e41c265da87ffd02
Craig Satchell <satchdev@gmail.com>
2021-05-10 23:25:34 -0400

    add main app functions

--- index.js
+++ index.js
@@ -7,5 +7,35 @@
 
 // generate day trip
 function generateDayTrip() {
-   document.getElementById("trip").innerHTML = '<p>This area is a placehold for the trip results.</p>';
+   const dest = selectDestination();
+   document.getElementById("trip").innerHTML = '<p>' + dest + '</p>';
 }
+
+function selectDestination() {
+   const arr = ['New York City',"Ocean City, NJ", "Philadelphia", "Washington, DC","Cherry Hill, New Jersey","Rehobeth Beach, Delaware"]
+   return 'New York City';
+}
+
+
+function selectRestaurant() {
+   const arr = ['Ruth Chris Steakhouse', 'Ben & Bonz Barbecue', 'True Thai Crusine', "Nature's Treats Dining Extravaganza", "Aunt Lula's Southern Style Cooking"]
+   return arr[selRandomItem(arr)];
+}
+
+function selectTransportMode() {
+   const arr = ['private vehicle', 'bus', 'train', 'Uber', 'Lyft'];
+   return arr[selRandomItem(arr)];
+}
+
+
+function selectEntertainment() {
+   const arr = ['movie', 'live musical', 'live play', 'jazz bar', 'night club'];
+   return arr[selRandomItem(arr)];
+}
+
+function selRandomItem(arr) {
+   let itemIndex = Math.floor(Math.random() * arr.length);
+   return itemIndex;
+}
+
+console.log(selRandomItem([1, 3, 4, 5, 6, 7, 8]));
