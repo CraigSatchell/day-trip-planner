e1af895d537ceb9eb9d90bb09c3f98b49bee0617
Craig Satchell <satchdev@gmail.com>
2021-05-11 00:27:26 -0400

    added compl. logic for trip generator

--- index.js
+++ index.js
@@ -4,30 +4,45 @@
 */
 
 "use strict";
+/* The code below could be refactored to use one function instead of five to generate trip details */
+
 
 // generate day trip
 function generateDayTrip() {
+   // select trip details
    const dest = selectDestination();
-   document.getElementById("trip").innerHTML = '<p>' + dest + '</p>';
+   const dining = selectRestaurant();
+   const transport = selectTransportMode();
+   const entertain = selectEntertainment();
+
+   // results template string
+   const html = "<div class='tripDetails'><ul><li><span>Destination:</span>&nbsp;&nbsp;" + dest + "</li><li><span>Restaurant:</span>&nbsp;&nbsp;" + dining + "</li><li><span>Travel Mode:</span>&nbsp;&nbsp;" + transport + "</li><li><span>Entertainment:</span>&nbsp;&nbsp;" + entertain + "</li></li></ul></div>"
+
+   // display trip results in browser
+   document.getElementById("trip").innerHTML = html;
+
+   console.log('Destination: ', dest, 'Restaurant: ', resturant, 'Transportation: ', transport, 'Entertainment: ', entertain);
 }
 
+// select trip destination
 function selectDestination() {
    const arr = ['New York City',"Ocean City, NJ", "Philadelphia", "Washington, DC","Cherry Hill, New Jersey","Rehobeth Beach, Delaware"]
-   return 'New York City';
-}
+   return arr[selRandomItem(arr)];}
 
-
+// select trip restaurant
 function selectRestaurant() {
    const arr = ['Ruth Chris Steakhouse', 'Ben & Bonz Barbecue', 'True Thai Crusine', "Nature's Treats Dining Extravaganza", "Aunt Lula's Southern Style Cooking"]
    return arr[selRandomItem(arr)];
 }
 
+// select trip transport mode
 function selectTransportMode() {
    const arr = ['private vehicle', 'bus', 'train', 'Uber', 'Lyft'];
    return arr[selRandomItem(arr)];
 }
 
 
+// select trip enterainment
 function selectEntertainment() {
    const arr = ['movie', 'live musical', 'live play', 'jazz bar', 'night club'];
    return arr[selRandomItem(arr)];
@@ -38,4 +53,3 @@ function selRandomItem(arr) {
    return itemIndex;
 }
 
-console.log(selRandomItem([1, 3, 4, 5, 6, 7, 8]));
