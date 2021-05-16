0339d32455384b4ea9d45dbf628f16f09b5743a1
Craig Satchell <satchdev@gmail.com>
2021-05-16 03:14:01 -0400

    refactored; feedback from JJ Vegas

--- index.js
+++ index.js
@@ -8,13 +8,16 @@
 
 // generate day trip
 function generateDayTrip() {
-   
-   // select trip details
-   const dest = selectDestination();
-   const restuarant = selectRestaurant();
-   const transport = selectTransportMode();
-   const entertain = selectEntertainment();
+   const arrLocation = ["Annapolis, Maryland", 'New York City', "Ocean City, New Jersey", "Philadelphia", "Washington, DC", "Cherry Hill, New Jersey", "Rehobeth Beach, Delaware"]
+   const arrEntertainment = ['movie', 'live musical', 'live play', 'jazz bar', 'night club'];
+   const arrTransportation = ['Private Vehicle', 'Bus', 'Amtrak', 'Uber', 'Lyft', 'Chartered Jet', 'Commercial Airlines'];
+   const arrRestaurant = ['Ruth Chris Steakhouse', 'Ben & Bonz Barbecue', 'True Thai Crusine', "Nature's Treats Dining Extravaganza", "Aunt Lula's Southern Style Cooking", "Emeril's Food Festival", "Food Lover's Palace", "Steaks and More", "Texas Roadhouse", "Longhorn Steakhouse"]
 
+   // select trip details
+   const dest = selectTripDetails(arrLocation);
+   const restuarant = selectTripDetails(arrRestaurant);
+   const transport = selectTripDetails(arrTransportation);
+   const entertain = selectTripDetails(arrEntertainment);
 
    // results template string
    const html = `<div class='tripDetails'><ul><li><span>Destination:</span>&nbsp;&nbsp;${dest}</li><li><span>Restaurant:</span>&nbsp;&nbsp;${restuarant}</li><li><span>Travel Mode:</span>&nbsp;&nbsp;${transport}</li><li><span>Entertainment:</span>&nbsp;&nbsp;${entertain}</li></li></ul></div>`
@@ -26,30 +29,13 @@ function generateDayTrip() {
    console.log('Destination: ', dest, 'Restaurant: ', restuarant, 'Transportation: ', transport, 'Entertainment: ', entertain);
 }
 
-// select trip destination
-function selectDestination() {
-   const arr = ["Annapolis, Maryland", 'New York City', "Ocean City, New Jersey", "Philadelphia", "Washington, DC", "Cherry Hill, New Jersey", "Rehobeth Beach, Delaware"]
-   return arr[selRandomItem(arr)];
-}
 
-// select trip restaurant
-function selectRestaurant() {
-   const arr = ['Ruth Chris Steakhouse', 'Ben & Bonz Barbecue', 'True Thai Crusine', "Nature's Treats Dining Extravaganza", "Aunt Lula's Southern Style Cooking", "Emeril's Food Festival", "Food Lover's Palace","Steaks and More","Texas Roadhouse","Longhorn Steakhouse"]
-   return arr[selRandomItem(arr)];
-}
-
-// select trip transport mode
-function selectTransportMode() {
-   const arr = ['Private Vehicle', 'Bus', 'Amtrak', 'Uber', 'Lyft','Chartered Jet','Commercial Airlines'];
+// select trip details
+function selectTripDetails(arr) {
    return arr[selRandomItem(arr)];
 }
 
 
-// select trip enterainment
-function selectEntertainment() {
-   const arr = ['movie', 'live musical', 'live play', 'jazz bar', 'night club'];
-   return arr[selRandomItem(arr)];
-}
 
 // return index of random item
 function selRandomItem(arr) {
@@ -65,7 +51,8 @@ document.addEventListener('click', (e) => {
       document.getElementById('actionButtons').classList.remove('hide');
 
    } else if (e.target.value === 'discard') {
-      window.location.reload();
+      //window.location.reload();
+      generateDayTrip();
 
    } else if (e.target.value === 'keep') {
       document.getElementById('actionButtons').innerHTML = "<h4 style='width: 300px;'>Congratulations! Nice choice. Enjoy your trip...</h4>";
